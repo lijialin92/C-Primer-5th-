e012118581013812fd8cd2966846911547f6a222
lijialin <lijialin990@gmail.com>
2020-05-28 19:22:33 +0200

    update 10.3, 10.4 and 10.5

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <vector>
 #include <list>
+#include <numeric>
+#include <string.h>
 
 /**
  * exercise 10.1
@@ -29,6 +31,39 @@ void exercise10_1()
      {
          std::cout << i << "\t";
      }
+}
+
+/**
+ * exercise 10.3
+ * call exercise10_3 in the main function and see the result*/
+void exercise10_3()
+{
+    std::vector<int> a{1, 3, 5, 54, 6, 7, 2, 4, 6, 4, 9, 38, 27, 20, 3, 4, 4};
+    std::cout << "sum of all the elements of array is: " << std::accumulate(a.cbegin(), a.cend(), 0);
+}
+
+/**
+ * exercise 10.4
+ * call exercise10_4 in the main function and see the result
+ * the third parameter of accumulate() defines the return data type and also to use which add operator. If the third
+ * parameter is 0 then the return of the accumulate() will also be int. Will loss accuracy. So the input should be 0.0*/
+void exercise10_4()
+{
+    std::vector<double> a{1.3, 3.9, 4.2, 4.5};
+    std::cout << "sum of all the elements of array is: " << std::accumulate(a.cbegin(), a.cend(), 0.0);
+}
+
+/**
+ * exercise 10.5
+ * string in C style is char*, then equal() will compare every char* pointer are they equal? So enough the content of
+ * the string could be the same but their pointer could also be different. Then they are not equal to each other.
+ * */
+void exercise10_5()
+{
+    char* a[] {"aaaa", "bbbb", "."};
+    char* b[] {strdup(a[0]), strdup(a[1]), strdup(a[2])};
+    std::cout << "are these two C style string the same?" << std::endl;
+    std::cout << std::boolalpha << std::equal(a, a+3, b);
 }
 
 int main()
